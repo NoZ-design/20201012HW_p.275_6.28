bb7c83b8c73f34e43cd6b87f85e7a24b98b1d280
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-10-12 16:42:11 +0800

    20201012HW_p.275_6.28

---
+++ 20201012HW_p.275_6.28/AutoPolicy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _20201012HW_p._275_6._28
+{
+    class AutoPolicy
+    {
+        public int AccountNumber { get; set; }
+        public string MakeAndModel { get; set; }
+        public string state;
+
+
+        public AutoPolicy (int accountNumber,string makeAndModel,string state)
+        {
+            AccountNumber = accountNumber;
+            MakeAndModel = makeAndModel;
+            State = state;
+        }
+        public bool IsNoFaultState
+        {
+            get
+            {
+                bool noFaultState;
+                switch (State)
+                {
+                    case "MA": case "CT": case "ME": case "NH": case "NJ": case "NY": case "PA": case "VT":
+                        noFaultState = true;
+                        break;
+                    default:
+                        noFaultState = false;
+                        break;
+                }
+                return noFaultState;
+            }
+        }
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+            set
+            {
+                if (value == "MA" || value == "CT" || value == "ME" || value == "NH" || value == "NJ" || value == "NY" || value == "PA" || value == "VT")
+                {
+                    state = value;
+                }
+                else
+                {
+                    state = "error";
+                }
+            }
+        }
+    }
+}
---
+++ 20201012HW_p.275_6.28/AutoPolicyTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _20201012HW_p._275_6._28
+{
+    class AutoPolicyTest
+    {
+        static void Main(string[] args)
+        {
+            AutoPolicy policy1 = new AutoPolicy(11111111, "Toyota", "NJ");
+            AutoPolicy policy2 = new AutoPolicy(22222222, "Ford", "MM");
+
+            PolicyInNoFaultState(policy1);
+            PolicyInNoFaultState(policy2);
+        }
+
+        public static void PolicyInNoFaultState(AutoPolicy policy)
+        {
+            Console.WriteLine("The auto policy:");
+            Console.Write($"Account#:{policy.AccountNumber};");
+            Console.WriteLine($"Car:{policy.MakeAndModel};");
+            Console.Write($"State{policy.State};");
+            Console.Write($"({(policy.IsNoFaultState ? "is":" is not ")})");
+            Console.WriteLine("a no-fault state\n");
+        }
+    }
+}
